336a1a0d3c29114a22b3230023d79f8da86527df
Nguyen Quoc Thang <yuginovaniac@gmail.com>
2023-09-27 16:26:56 +0700

    5. Frontend Exercises (KnockoutJS)

---
+++ Magenest/KnockoutJS/Block/Test.php
@@ -0,0 +1,31 @@
+<?php
+namespace Magenest\KnockoutJS\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Test extends Template
+{
+    /**
+     * @var array|\Magento\Checkout\Block\Checkout\LayoutProcessorInterface[]
+     */
+    protected $layoutProcessors;
+
+    public function __construct(
+        Template\Context $context,
+        array $layoutProcessors = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->jsLayout = isset($data['jsLayout']) && is_array($data['jsLayout']) ? $data['jsLayout'] : [];
+        $this->layoutProcessors = $layoutProcessors;
+    }
+
+    public function getJsLayout()
+    {
+        foreach ($this->layoutProcessors as $processor) {
+            $this->jsLayout = $processor->process($this->jsLayout);
+        }
+        return json_encode($this->jsLayout);
+    }
+
+}
